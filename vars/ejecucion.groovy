791ebfa7676bf57a49574d8a19591d86eebc7bd4
Flamel Canto <flamel.canto@deepsource.cl>
2022-01-31 20:11:27 -0300

    añadidos en vars

---
+++ vars/ejecucion.groovy
@@ -0,0 +1,44 @@
+/*
+
+    forma de invocación de método call:
+
+    def ejecucion = load 'script.groovy'
+    ejecucion.call()
+
+*/
+
+def call(){
+pipeline {
+    agent any
+    environment {
+        NEXUS_USER_VAR      = credentials('NEXUS-USER')
+        NEXUS_USER_PASS_VAR = credentials('NEXUS-PASS')
+    }
+    parameters {
+        choice(
+            name:'compileTool',
+            choices: ['Maven', 'Gradle'],
+            description: 'Seleccione herramienta de compilacion'
+        )
+    }
+    stages {
+        stage("Pipeline"){
+            steps {
+                script{
+                  switch(params.compileTool)
+                    {
+                        case 'Maven':
+                            def ejecucion = load 'maven.groovy'
+                            ejecucion.call()
+                        break;
+                        case 'Gradle':
+                            def ejecucion = load 'gradle.groovy'
+                            ejecucion.call()
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+return this;
